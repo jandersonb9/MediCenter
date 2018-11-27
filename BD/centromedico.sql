b9e931dcab25f919da70a66da55b0fda1375bb14
jBarrientosDF <jBarrientos@demandfrontier.com>
2018-11-27 10:51:54 -0500

    MediCenter

---
+++ BD/centromedico.sql
@@ -0,0 +1,225 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-11-2018 a las 16:49:09
+-- Versión del servidor: 10.1.35-MariaDB
+-- Versión de PHP: 7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `centromedico`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `citas`
+--
+
+CREATE TABLE `citas` (
+  `idcita` int(11) NOT NULL,
+  `citfecha` date NOT NULL,
+  `cithora` time NOT NULL,
+  `citPaciente` int(11) NOT NULL,
+  `citMedico` int(11) NOT NULL,
+  `citConsultorio` int(11) NOT NULL,
+  `citestado` enum('Asignado','atendido') COLLATE utf8_spanish_ci NOT NULL,
+  `citobservaciones` text COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `consultorios`
+--
+
+CREATE TABLE `consultorios` (
+  `idConsultorio` int(11) NOT NULL,
+  `conNombre` char(15) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `consultorios`
+--
+
+INSERT INTO `consultorios` (`idConsultorio`, `conNombre`) VALUES
+(1, 'Pediatria');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especialidades`
+--
+
+CREATE TABLE `especialidades` (
+  `idespecialidad` int(11) NOT NULL,
+  `espNombre` char(15) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `especialidades`
+--
+
+INSERT INTO `especialidades` (`idespecialidad`, `espNombre`) VALUES
+(1, 'cirujano');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicos`
+--
+
+CREATE TABLE `medicos` (
+  `idMedico` int(11) NOT NULL,
+  `medidentificacion` char(15) COLLATE utf8_spanish_ci NOT NULL,
+  `mednombres` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `medapellidos` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `medEspecialidad` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `medtelefono` char(15) COLLATE utf8_spanish_ci NOT NULL,
+  `medcorreo` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `medicos`
+--
+
+INSERT INTO `medicos` (`idMedico`, `medidentificacion`, `mednombres`, `medapellidos`, `medEspecialidad`, `medtelefono`, `medcorreo`) VALUES
+(1, '', 'Anderson', 'Barrientos', '', '', ''),
+(2, '10154', 'trident', 'trident', 'pediatra', '31042281464', 'trident@correo.co');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pacientes`
+--
+
+CREATE TABLE `pacientes` (
+  `idPaciente` int(11) NOT NULL,
+  `pacIdentificacion` char(15) COLLATE utf8_spanish_ci NOT NULL,
+  `pacNombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `pacApellidos` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `pacFechaNacimiento` date NOT NULL,
+  `pacSexo` enum('Femenino','Masculino') COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `pass` varchar(200) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `nombres` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `apellidos` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Roll` varchar(15) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `usuario`, `pass`, `nombres`, `apellidos`, `Roll`) VALUES
+(1, 'admin', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'Anderson', 'Barrientos', 'admin');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `citas`
+--
+ALTER TABLE `citas`
+  ADD PRIMARY KEY (`idcita`),
+  ADD KEY `cithora` (`cithora`),
+  ADD KEY `idPaciente` (`citPaciente`),
+  ADD KEY `idMedico` (`citMedico`),
+  ADD KEY `idConsultorio` (`citConsultorio`);
+
+--
+-- Indices de la tabla `consultorios`
+--
+ALTER TABLE `consultorios`
+  ADD PRIMARY KEY (`idConsultorio`),
+  ADD UNIQUE KEY `conNombre` (`conNombre`);
+
+--
+-- Indices de la tabla `especialidades`
+--
+ALTER TABLE `especialidades`
+  ADD PRIMARY KEY (`idespecialidad`),
+  ADD UNIQUE KEY `espNombre` (`espNombre`);
+
+--
+-- Indices de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  ADD PRIMARY KEY (`idMedico`),
+  ADD UNIQUE KEY `medidentificacion` (`medidentificacion`);
+
+--
+-- Indices de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`idPaciente`),
+  ADD UNIQUE KEY `pacIdentificacion` (`pacIdentificacion`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario` (`usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `consultorios`
+--
+ALTER TABLE `consultorios`
+  MODIFY `idConsultorio` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `especialidades`
+--
+ALTER TABLE `especialidades`
+  MODIFY `idespecialidad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  MODIFY `idMedico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `pacientes`
+--
+ALTER TABLE `pacientes`
+  MODIFY `idPaciente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
